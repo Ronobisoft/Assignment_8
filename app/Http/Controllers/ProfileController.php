bc44f364916c6ed8c7e7c200d3f0e7110bdecbe3
Rafiqun <rafiqunnobi786@gmail.com>
2023-11-18 21:39:52 +0600

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    Public function index($id){
+
+        $name ="Donal Trump";
+        $age ="75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+        
+        
+    $name = 'access_token';
+
+
+    $value = '123-XYZ';
+
+
+    $minutes = '1';
+
+
+    $path = '/';
+
+
+    $domain =  $_SERVER['SERVER_NAME'];
+
+
+    $secure = 'false ';
+
+
+    $httpOnly = 'true';
+
+    return response()->json($data);
+
+    return response()->setStatusCode(200);
+
+
+    
+    
+    }
+}
